4bb428bd0054609da3fb06d921f27353963067bd
vkhajanchi <vihaan.khajanchi@gmail.com>
2025-02-10 16:05:43 -0500

    added grain outer radius

--- rocket_py/polaris1/testrocket.py
+++ rocket_py/polaris1/testrocket.py
@@ -5,19 +5,19 @@ test_rocket = Rocket(
     mass=6.658,
     inertia=(2.222, 2.222, 0.00532),
     power_off_drag=0.70,
-    power_on_drag="./sim/test_rocket/data/test.csv",
+    power_on_drag="./rocket_py/polaris1/data/test.csv",
     center_of_mass_without_motor=1.13,  # need
     coordinate_system_orientation="nose_to_tail",
 )
 
 test_solid = SolidMotor(
-    thrust_source="./sim/test_rocket/data/Loki_L1482-LB.csv",
+    thrust_source="./rocket_py/polaris1/data/Loki_L1482-LB.csv",
     dry_mass=1.724,
     dry_inertia=(0.0361, 0.0361, 0.00124),
     nozzle_radius=0.0381,
     grain_number=3,
     grain_density=1815,  # need
-    grain_outer_radius=33 / 1000,  # need
+    grain_outer_radius=38 / 1000,
     grain_initial_inner_radius=0.009525,
     grain_initial_height=0.13335,
     grain_separation=5 / 1000,  # need
