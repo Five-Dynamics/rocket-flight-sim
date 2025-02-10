09e82e7782bf711c8162c27b7239273aaefeaddb
Darin Tsai <darint916@gmail.com>
2025-02-10 16:21:21 -0500

    bruh pathing fix

--- rocket_py/polaris1/testrocket.py
+++ rocket_py/polaris1/testrocket.py
@@ -4,6 +4,7 @@ testrocket.py
 This module defines and simulates a rocket (Polaris 1) using the RocketPy library and specs
 from the aae me teams
 """
+
 from rocketpy import Rocket, SolidMotor
 
 test_rocket = Rocket(
